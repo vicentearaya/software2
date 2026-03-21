804df9f5a8abf9ff27c0d88000af3f6495896b4e
Vicente Nicolas Araya Díaz <miilwaukee@192.168.1.30>
2026-03-21 11:59:28 -0300

    refactor: Organizacion del repositorio

---
+++ frontend/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/miilwaukee/Documents/Software\ 2/aplicacion_proyecto/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/miilwaukee/Documents/Software\ 2/aplicacion_proyecto/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
