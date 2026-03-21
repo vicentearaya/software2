4cee0080f90df237da7f336fe237992c25310676
Vicente Nicolas Araya Díaz <miilwaukee@192.168.1.30>
2026-03-21 12:18:18 -0300

    chore: complete flutter files move into frontend/

---
+++ frontend/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/miilwaukee/Documents/Software\ 2/aplicacion_proyecto/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/miilwaukee/Documents/Software\ 2/aplicacion_proyecto/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
